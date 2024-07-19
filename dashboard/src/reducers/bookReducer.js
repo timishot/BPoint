e7b30723c8231fad374f49449e5d682f3cb206e9
Timishot <timidotcom58@gmail.com>
2024-07-19 13:32:19 +0100

    fix

--- dashboard/src/reducers/bookReducer.js
+++ dashboard/src/reducers/bookReducer.js
@@ -1,3 +1,19 @@
+// import { FETCH_BOOKS_SUCCESS } from '../actions/bookActionTypes';
+// import { fromJS } from 'immutable';
+
+// export const initialState = fromJS({
+//   books: [],
+// });
+
+// export function bookReducer(state = initialState, action = { type: null }) {
+//   switch (action.type) {
+//     case FETCH_BOOKS_SUCCESS:
+//       return state.set('books', fromJS(action.payload));
+//     default:
+//       return state;
+//   }
+// }
+
 import { FETCH_BOOKS_SUCCESS } from '../actions/bookActionTypes';
 import { fromJS } from 'immutable';
 
@@ -8,7 +24,9 @@ export const initialState = fromJS({
 export function bookReducer(state = initialState, action = { type: null }) {
   switch (action.type) {
     case FETCH_BOOKS_SUCCESS:
-      return state.set('books', fromJS(action.payload));
+      // Convert the Immutable.js object to a plain JavaScript object
+      const books = action.payload.toJS ? action.payload.toJS() : action.payload;
+      return state.set('books', fromJS(books));
     default:
       return state;
   }
